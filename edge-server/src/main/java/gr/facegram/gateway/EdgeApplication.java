a3cffde1dcc8a69ce23177f9a6741842db3766d9
nicolasmanic <nicolas.kanakis@gmail.com>
2017-05-04 23:53:15 +0300

    fix Edge Server - Eureka communication

--- edge-server/src/main/java/gr/facegram/gateway/EdgeApplication.java
+++ edge-server/src/main/java/gr/facegram/gateway/EdgeApplication.java
@@ -3,8 +3,8 @@ package gr.facegram.gateway;
 import gr.facegram.gateway.filters.AccessLogFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class EdgeApplication {
 
     public static void main(String[] args) {
