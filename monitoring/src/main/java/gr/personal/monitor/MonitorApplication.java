a6843c99317d20a7f3f94b434603b998ebcca83a
nicolasmanic <nicolas.kanakis@gmail.com>
2017-05-07 19:55:20 +0300

    Monitoring module initial commit

---
+++ monitoring/src/main/java/gr/personal/monitor/MonitorApplication.java
@@ -0,0 +1,25 @@
+package gr.personal.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Nick Kanakis on 6/5/2017.
+ */
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
+@EnableTurbine
+public class MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+
+}
