cc50cf2c2eb6c5bcc4d2a6cfb224a193c2d1605a
nicolasmanic <nicolas.kanakis@gmail.com>
2017-05-16 00:32:12 +0300

    add unit testing infrastructure for user controller & service

---
+++ user-service/src/test/java/gr/personal/user/controller/HomepageControllerTest.java
@@ -0,0 +1,13 @@
+package gr.personal.user.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Nick Kanakis on 15/5/2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomepageControllerTest {
+}
