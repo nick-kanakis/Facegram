ff40c99b9916b2bd1caa98c89a3358b3022191c2
nkanakis <nicolas.kanakis@gmail.com>
2017-06-14 16:00:18 +0300

    Group service initial commit

---
+++ group-service/src/main/java/gr/personal/group/GroupApplication.java
@@ -0,0 +1,15 @@
+package gr.personal.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by nkanakis on 6/14/2017.
+ */
+@SpringBootApplication
+public class GroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupApplication.class, args);
+    }
+}
